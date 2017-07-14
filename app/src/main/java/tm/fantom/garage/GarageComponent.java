dd30267e20f66f6fcd6352e6bf0297cf118a53e1
FantoM <freereg@ukr.net>
2017-07-14 20:22:03 +0300

    cleanup)))

--- app/src/main/java/tm/fantom/garage/GarageComponent.java
+++ app/src/main/java/tm/fantom/garage/GarageComponent.java
@@ -4,6 +4,7 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 import tm.fantom.garage.ui.DriversFragment;
+import tm.fantom.garage.ui.EditDriverFragment;
 import tm.fantom.garage.ui.EditVehicleFragment;
 import tm.fantom.garage.ui.NewDriverFragment;
 import tm.fantom.garage.ui.NewVehicleFragment;
@@ -16,12 +17,9 @@ import tm.fantom.garage.ui.VehiclesFragment;
 @Component(modules = GarageModule.class)
 public interface GarageComponent {
     void inject(DriversFragment fragment);
-
     void inject(VehiclesFragment fragment);
-
     void inject(NewVehicleFragment fragment);
-
     void inject(NewDriverFragment fragment);
-
     void inject(EditVehicleFragment fragment);
+    void inject(EditDriverFragment fragment);
 }
